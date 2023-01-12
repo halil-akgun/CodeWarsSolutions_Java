8a34e044ee18ac86bf48797967b1f11d2af158b4
halil <halilakgun0@gmail.com>
2023-01-12 17:25:28 +0300

    CW08_ReversedWords

--- src/CW08_ReversedWords.java
+++ src/CW08_ReversedWords.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class CW08_ReversedWords {
+/*
+Complete the solution so that it reverses all of the words within the string passed in.
+Words are separated by exactly one space and there are no leading or trailing spaces.
+
+Example(Input --> Output):
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+ */
+public class CW08_ReversedWords {
+    public static void main(String[] args) {
+        String str = "The greatest victory is that which requires no battle";
+        System.out.println(reverseWords(str));
+    }
+
+    public static String reverseWords(String str) {
+        String[] arr = str.split(" ");
+        String strReverse = "";
+        for (int i = arr.length - 1; i >= 0; i--) {
+            strReverse += arr[i] + " ";
+        }
+        return strReverse.substring(0, strReverse.length() - 1);
+    }
 }
