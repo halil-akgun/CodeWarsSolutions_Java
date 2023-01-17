502d263d5db5fcf2e66948254efc2b88f1de22a2
halil <halilakgun0@gmail.com>
2023-01-17 17:19:40 +0300

    CW09_EvenOrOdd

---
+++ src/CW09_EvenOrOdd.java
@@ -0,0 +1,12 @@
+/*
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+ */
+public class CW09_EvenOrOdd {
+    public static void main(String[] args) {
+        System.out.println(even_or_odd(3));
+    }
+    public static String even_or_odd(int number) {
+        if (number%2==0) return "Even";
+        return "Odd";
+    }
+}
