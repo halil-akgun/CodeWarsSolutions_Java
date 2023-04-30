c37945dcf131965ddfbbd0b68a2f1e2d3f090dba
halil <halilakgun0@gmail.com>
2023-04-30 19:13:13 +0300

    CW33_YouCantCodeUnderPressure1

---
+++ src/CW33_YouCantCodeUnderPressure1.java
@@ -0,0 +1,12 @@
+/*
+Code as fast as you can! You need to double the integer and return it.
+ */
+public class CW33_YouCantCodeUnderPressure1 {
+    public static void main(String[] args) {
+        System.out.println(doubleInteger(5));
+    }
+
+    public static int doubleInteger(int i) {
+        return i * 2;
+    }
+}
