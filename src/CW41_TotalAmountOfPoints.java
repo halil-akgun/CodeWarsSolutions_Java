664586e22b8fb43d7e216e5bd69ff3fae91e5ca5
halil <halilakgun0@gmail.com>
2023-05-15 20:39:09 +0300

    CW41_TotalAmountOfPoints

---
+++ src/CW41_TotalAmountOfPoints.java
@@ -0,0 +1,41 @@
+/*
+Our football team has finished the championship.
+
+Our team's match results are recorded in a collection of strings. Each match is represented
+by a string in the format "x:y", where x is our team's score, and y is our opponents score.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Points are awarded for each match as follows:
+
+if x > y: 3 points (win)
+if x < y: 0 points (loss)
+if x = y: 1 point (tie)
+We need to write a function that takes this collection and returns the number of points
+our team (x) got in the championship by the rules given above.
+
+Notes:
+
+our team always plays 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4
+ */
+public class CW41_TotalAmountOfPoints {
+    public static void main(String[] args) {
+        String[] arr = {"1:0", "2:0", "3:0", "4:4", "2:2", "3:3", "1:4", "2:3", "2:4", "3:4"};
+        System.out.println(points(arr));
+    }
+
+    public static int points(String[] games) {
+        int score = 0;
+        int x;
+        int y;
+        for (String w : games) {
+            x = Integer.parseInt(w.substring(0, 1));
+            y = Integer.parseInt(w.substring(2));
+            if (x > y) score += 3;
+            else if (x == y) score += 1;
+        }
+        return score;
+    }
+}
