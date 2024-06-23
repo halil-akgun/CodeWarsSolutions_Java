baf2ba62c698b255a3a5ab9b0b4e3a536ed48228
Halil Akgun <halilakgun0@gmail.com>
2024-06-24 00:40:36 +0200

    CW42_EvaluateTheGroupOfParentheses

--- src/_6kyu/CW42_EvaluateTheGroupOfParentheses.java
+++ src/_6kyu/CW42_EvaluateTheGroupOfParentheses.java
@@ -1,5 +1,10 @@
 package _6kyu;
 
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /*
 The function is given a non-empty balanced parentheses string. Each opening '(' has a corresponding closing ')'.
 Compute the total score based on the following rules:
@@ -34,37 +39,53 @@ eval_parentheses("()((()))") ➞ 5
 public class CW42_EvaluateTheGroupOfParentheses {
     public static void main(String[] args) {
         System.out.println(evalParentheses("()"));
-        System.out.println(evalParentheses("(())"));
+        System.out.println(evalParentheses("((()))"));
         System.out.println(evalParentheses("()()"));
         System.out.println(evalParentheses("((())())"));
-        System.out.println(evalParentheses("(()(()))")); // not passed
+        System.out.println(evalParentheses("(()(()))"));
         System.out.println(evalParentheses("()(())"));
         System.out.println(evalParentheses("()((()))"));
+        System.out.println(evalParentheses("()(((((())))))"));
     }
 
     public static int evalParentheses(String s) {
 
-        int result = 0;
-        int openParentheses = 0;
-        int temp = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                openParentheses++;
-            } else {
-                if (i != 0 && s.charAt(i - 1) == '(') {
-                    temp++;
-                } else {
-                    temp *= 2;
-                }
-                openParentheses--;
-                if (openParentheses == 0) {
-                    result += temp;
-                    temp = 0;
-                }
-            }
+        while (s.contains("()")) {
+            s = s.replace("()", "+1");
         }
 
-        return result;
+        Pattern pattern = Pattern.compile("\\(([^()]+)\\)");
+        Matcher matcher;
+
+        while ((matcher = pattern.matcher(s)).find()) {
+            String group = matcher.group(1);
+
+            if (group.length() - group.replaceAll("\\+", "").length() > 1) {
+                group = Arrays.stream(group.substring(1).split("\\+")).mapToInt(Integer::parseInt).sum() + "";
+            } else if (group.contains("+")) {
+                group = group.substring(1);
+            }
+
+            int value = Integer.parseInt(group);
+            int doubleValue = value * 2;
+            s = matcher.replaceFirst("+" + doubleValue);
+        }
+
+        if (s.charAt(0) == '+') {
+            String[] a = s.substring(1).split("\\+");
+            return Arrays.stream(s.substring(1).split("\\+")).mapToInt(Integer::parseInt).sum();
+        }
+
+        return Arrays.stream(s.split("\\+")).mapToInt(Integer::parseInt).sum();
+
+
+        // LetMeeDie, Elsa123 :
+//        Stack<Integer> st = new Stack(); st.push(0);
+//        for (char c : s.toCharArray()) {
+//            if (c == ')')
+//                st.push(Math.max(1, st.pop() * 2) + st.pop());
+//            else st.push(0);
+//        }
+//        return st.pop();
     }
 }
