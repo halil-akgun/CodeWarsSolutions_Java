a91c590e3ae651a64f6430e5713fce81a6ce3fb5
Halil Akgun <halilakgun0@gmail.com>
2024-07-11 17:35:45 +0200

    CW09_LastDigitOfALargeNumber

---
+++ src/_5kyu/CW09_LastDigitOfALargeNumber.java
@@ -0,0 +1,18 @@
+package _5kyu;
+
+/*
+Define a function that takes in two non-negative integers 𝑎 and 𝑏 and returns the last decimal digit of 𝑎^𝑏.
+Note that 𝑎 and 𝑏 may be very large!
+
+For example, the last decimal digit of 9^7 is 9, since 9^7=4782969. The last decimal digit of (2^200)^2^300, which has over 10^92 decimal digits, is 6. Also, please take 0^0  to be 1.
+
+You may assume that the input will always be valid.
+
+Examples
+lastDigit(new BigInteger("4"), new BigInteger("1")) // returns 4
+lastDigit(new BigInteger("4"), new BigInteger("2")) // returns 6
+lastDigit(new BigInteger("9"), new BigInteger("7")) // returns 9
+lastDigit(new BigInteger("10"), new BigInteger("10000000000")) // returns 0
+ */
+public class CW09_LastDigitOfALargeNumber {
+}
