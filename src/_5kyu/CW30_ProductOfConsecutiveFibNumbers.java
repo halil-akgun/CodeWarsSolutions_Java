7414ec0e31b58258e37dfa538eae13e171328a17
Halil Akgun <halilakgun0@gmail.com>
2024-08-10 18:11:48 +0200

    CW30_ProductOfConsecutiveFibNumbers

---
+++ src/_5kyu/CW30_ProductOfConsecutiveFibNumbers.java
@@ -0,0 +1,60 @@
+package _5kyu;
+
+import java.util.Arrays;
+
+/*
+The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+such as
+
+F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+F(n) * F(n+1) = prod.
+
+Your function productFib takes an integer (prod) and returns an array:
+
+[F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+depending on the language if F(n) * F(n+1) = prod.
+
+If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+[F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+F(n) being the smallest one such as F(n) * F(n+1) > prod.
+
+Some Examples of Return: (depend on the language)
+productFib(714) # should return (21, 34, true),
+                # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+productFib(800) # should return (34, 55, false),
+                # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+-----
+productFib(714) # should return [21, 34, true],
+productFib(800) # should return [34, 55, false],
+-----
+productFib(714) # should return {21, 34, 1},
+productFib(800) # should return {34, 55, 0},
+-----
+productFib(714) # should return {21, 34, true},
+productFib(800) # should return {34, 55, false},
+ */
+public class CW30_ProductOfConsecutiveFibNumbers {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(productFib(714))); // [21, 34, 1]
+        System.out.println(Arrays.toString(productFib(800))); // [34, 55, 0]
+    }
+
+    public static long[] productFib(long prod) {
+        return calculateProductFib(0, 1, prod);
+    }
+
+    private static long[] calculateProductFib(long first, long second, long target) {
+        long product = first * second;
+        if (product > target) return new long[]{first, second, 0};
+        if (product == target) return new long[]{first, second, 1};
+        return calculateProductFib(second, first + second, target);
+    }
+}
